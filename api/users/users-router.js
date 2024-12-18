4f3040e6744b0075fefabc329830df3a286f8dd4
Conner Krenzer <krenzer84@gmail.com>
2024-12-18 10:08:03 -0500

    PUT

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -21,40 +21,37 @@ router.get("/", logger, (req, res, next) => {
 
 router.get("/:id", validateUserId, (req, res) => {
   // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
-  console.log(req.user);
+  res.json(req.user);
 });
 
-router.post("/", validateUser, (req, res) => {
+router.post("/", validateUser, (req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
   console.log(req.name);
+  User.insert({ name: req.name })
+    .then((user) => res.status(201).json(user))
+    .catch(next);
 });
 
-router.put("/:id", validateUserId, validateUser, (req, res) => {
+router.put("/:id", validateUserId, validateUser, (req, res, next) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
-  console.log(req.user);
-  console.log(req.name);
+  User.update(req.params.id, { name: req.name })
+    .then((updatedUser) => {
+      res.json(updatedUser);
+    })
+    .catch(next);
 });
 
 router.delete("/:id", validateUserId, (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
-  // this needs a middleware to verify user id
 });
 
 router.get("/:id/posts", validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
-  // this needs a middleware to verify user id
 });
 
 router.post("/:id/posts", validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
-  console.log(req.user);
-  console.log(req.text);
 });
 
 router.use((error, req, res, next) => {

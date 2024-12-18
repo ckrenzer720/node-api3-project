8940d1074af2ecba0131ab3d720e364f4bdc713c
Conner Krenzer <krenzer84@gmail.com>
2024-12-18 09:56:49 -0500

    GET

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const User = require("./users-model");
+const Posts = require("../posts/posts-model");
 const {
   logger,
   validateUserId,
@@ -8,8 +10,13 @@ const {
 
 const router = express.Router();
 
-router.get("/", logger, (req, res) => {
+router.get("/", logger, (req, res, next) => {
   // RETURN AN ARRAY WITH ALL THE USERS
+  User.get()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch(next);
 });
 
 router.get("/:id", validateUserId, (req, res) => {
@@ -42,10 +49,19 @@ router.get("/:id/posts", validateUserId, (req, res) => {
   // this needs a middleware to verify user id
 });
 
-router.post("/:id/posts", validateUserId, (req, res) => {
+router.post("/:id/posts", validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  console.log(req.user);
+  console.log(req.text);
+});
+
+router.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    customMessage: "uh oh! something happened inside posts router",
+    message: error.message,
+  });
 });
 
 // do not forget to export the router
